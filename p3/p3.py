f839ecddb9b17e3a7c0667f4193fd6202201c419
Guy Matz <gmatz@matz.org>
2023-05-08 09:56:22 -0400

    what's with zip?

--- p3/p3.py
+++ p3/p3.py
@@ -5,53 +5,59 @@ from pyspark.mllib.tree import RandomForest, RandomForestModel
 from pyspark.mllib.util import MLUtils
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.sql.functions import when
+import sys
 
 DATA_FILE="data.csv"
 
 # Create a Spark Session
-ss = SparkSession.builder.master("local[*]").getOrCreate()
+if 'spark' not in dir():
+    spark = SparkSession.builder.master("local[*]").getOrCreate()
 
 # Load and parse the data file into an RDD
 data = spark.read.csv(DATA_FILE,header=True, sep=",")
+print("--- Data with original tex label: \n", data.take(1))
 
-
-#  APPROACH 1
-# I don't think we need IDs
-data = data.drop(data.id)
 # Change diagnosis to 0/1
 data = data.withColumn("diagnosis",
                        when(data.diagnosis == "M", 1)
                       .when(data.diagnosis == "B", 0)
        )
+print("--- Data with label replaced with 0/1: \n", data.take(1))
 # Split the data into training and test sets (30% held out for testing)
 labeled_data = data.rdd.map(lambda row:LabeledPoint(row[1], [row[2:]]))
+print("--- labeled data: \n", labeled_data.take(1))
 (training_data, test_data) = labeled_data.randomSplit([0.7, 0.3])
-
-### APPROACH 2
-##from pyspark.ml.feature import VectorAssembler
-### Then something like
-##va = VectorAssembler(inputCols = data.columns, outputCol='features')
-### see https://www.datatechnotes.com/2021/12/mllib-random-forest-classification.html
-
+print("--- test data: \n", test_data.take(1))
 
 # Train a RandomForest model.
-#  Empty categoricalFeaturesInfo indicates all features are continuous.
-#  Note: Use larger numTrees in practice.
-#  Setting featureSubsetStrategy="auto" lets the algorithm choose.
 model = RandomForest.trainClassifier(training_data,
             numClasses=2, categoricalFeaturesInfo={},
             numTrees=3, featureSubsetStrategy="auto",
             impurity='gini', maxDepth=4, maxBins=32)
+print("--- A bit of the model: \n", model.toDebugString().split('\n')[0:10])
 
-# Evaluate model on test instances and compute test error
-predictions = model.predict(test_data.map(lambda x: x.features))
-labels_predictions = test_data.map(lambda lp: lp.label).zip(predictions)
-testErr = labels_predictions.filter(
-    lambda lp: lp[0] != lp[1]).count() / float(test_data.count())
-print('Test Error = ' + str(testErr))
-print('Learned classification forest model:')
-print(model.toDebugString())
+predictionAndLabels = test_data.map(lambda lp: (float(model.predict(lp.features)), lp.label))
+predictionAndLabels.first()
 
-# Save and load model
-model.save(sc, "myRandomForestClassificationModel")
-sameModel = RandomForestModel.load(sc, "myRandomForestClassificationModel")
+
+#sys.exit(1)
+## Evaluate model on test instances and compute test error
+#### Old approach
+#predictions = model.predict(test_data.map(lambda x: x.features))
+#print(predictions.take(3))
+#test_data_labels = test_data.map(lambda lp: float(lp.label))
+#print(test_data.take(3))
+#print(test_data_labels.take(3))
+#labels_predictions = test_data_labels.zip(predictions)
+#print(labels_predictions.take(3)) 
+#sys.exit(1)
+#
+#testErr = labels_predictions.filter(
+#    lambda lp: lp[0] != lp[1]).count() / float(test_data.count())
+#print('Test Error = ' + str(testErr))
+#print('Learned classification forest model:')
+#print(model.toDebugString())
+#
+## Save and load model
+#model.save(sc, "myRandomForestClassificationModel")
+#sameModel = RandomForestModel.load(sc, "myRandomForestClassificationModel")
